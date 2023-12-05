8e2d0d8acd7646c8bba91e2429f7ff9a938bf842
Yevhen3000 <152332659+Yevhen3000@users.noreply.github.com>
2023-12-05 23:48:12 +0300

    Update ProjectFantasyGreatBritishBakeOffLeague.java

--- src/projectfantasygreatbritishbakeoffleague/ProjectFantasyGreatBritishBakeOffLeague.java
+++ src/projectfantasygreatbritishbakeoffleague/ProjectFantasyGreatBritishBakeOffLeague.java
@@ -15,6 +15,7 @@ public class ProjectFantasyGreatBritishBakeOffLeague {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        234
     }
     
 }
